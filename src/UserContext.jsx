a020fdef0f3a30e39cbd46e2ca9bbb37fb7b9589
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-04-28 10:49:49 -0300

    login and logout authentication

---
+++ src/UserContext.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
+import { useNavigate } from 'react-router-dom';
+
+export const UserContext = React.createContext();
+
+export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const navigate = useNavigate();
+
+  const userLogout = React.useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem('token');
+      navigate('/login');
+    },
+    [navigate]
+  );
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+
+    const userResponse = await fetch(url, options);
+    const userToken = await userResponse.json();
+    setData(userToken);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = TOKEN_POST({ username, password });
+
+      const tokenResponse = await fetch(url, options);
+      const { token } = await tokenResponse.json();
+      if (!tokenResponse.ok) {
+        throw new Error(`Error: ${tokenResponse.statusText}`);
+      }
+      window.localStorage.setItem('token', token);
+      await getUser(token);
+      navigate('/conta');
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Token Inválido');
+          await getUser(token);
+        } catch (err) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, [userLogout]);
+
+  return (
+    <UserContext.Provider
+      value={{ userLogin, userLogout, data, error, loading, login }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
