45fa9c738f67c02c60c619d759e1683e16bd86de
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-05-04 19:57:40 -0300

    add userHeader and options header

---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from '../User/UserHeaderNav.module.css';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import { ReactComponent as MyPhotos } from '../../Assets/feed.svg';
+import { ReactComponent as Stats } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AddPhoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Exit } from '../../Assets/sair.svg';
+
+const UserHeaderNav = () => {
+  const [mobile, setMobile] = React.useState(null);
+  const { userLogout } = React.useContext(UserContext);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    userLogout();
+    navigate('/login');
+  }
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MyPhotos />
+        {mobile && 'Minhas Fotos'}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Stats />
+        {mobile && 'Estatísticas'}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AddPhoto />
+        {mobile && 'Adicionar Foto'}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <Exit />
+        {mobile && 'Sair'}
+      </button>
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
